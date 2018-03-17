2aa5a24e8817e0a8290606bedd9d3d26b3c98f26
Taka Toizumi <ttoizmi>
2018-03-17 16:53:08 +0900

    add cvae.py

--- cvae.py
+++ cvae.py
@@ -14,7 +14,7 @@ from keras.datasets import mnist
 from keras.layers import Input
 from keras.layers import Dense
 from keras.layers import Lambda
-
+from keras.layers.merge import concatenate
 
 def main():
     sns.set_context('talk', font_scale=1.5)
@@ -22,16 +22,22 @@ def main():
     mpl.rc('image', cmap='inferno', interpolation='nearest') # for image
     pass
 
-class VAE():
-    def __init__(self):
 
+class CVAE():
+    def __init__(self):
         self.input_dim = 784
-        self.latent_dim = 2
+        self.latent_dim = 128
         nch = 256
 
-        # encoder
+        # input
         self.x_input = Input(shape=(self.input_dim,))
+        self.label_input_enc = Input(shape=(10,))
+        self.label_input_dec = Input(shape=(10,))
+
+        # encoder
         x = Dense(nch, activation='relu')(self.x_input)
+        lx = Dense(nch, activation='relu')(self.label_input_enc)
+        x = concatenate([x, lx])
         x = Dense(nch, activation='relu')(x)
 
         self.z_mean = Dense(self.latent_dim)(x)
@@ -40,10 +46,13 @@ class VAE():
 
         # decoder
         self.dec1 = Dense(nch, activation='relu')
+        self.dec1_label = Dense(nch, activation='relu')
         self.dec2 = Dense(nch, activation='relu')
         self.dec_out = Dense(self.input_dim, activation='sigmoid')
 
         x = self.dec1(self.z)
+        lx = self.dec1_label(self.label_input_dec)
+        x = concatenate([x, lx])
         x = self.dec2(x)
         self.x_out = self.dec_out(x)
 
@@ -54,18 +63,21 @@ class VAE():
         eps = K.random_normal(shape=(nd, nc), mean=0., stddev=1.0)
         return z_mean + K.exp(z_log_var / 2) * eps
 
-    def vae(self):
-        return Model(self.x_input, self.x_out)
+    def cvae(self):
+        return Model([self.x_input, self.label_input_enc, self.label_input_dec], self.x_out)
 
     def encoder(self):
-        return Model(self.x_input, self.z_mean)
+        return Model([self.x_input, self.label_input_enc], self.z_mean)
 
     def decoder(self):
         z = Input(shape=(self.latent_dim,))
+        l = Input(shape=(10,))
         x = self.dec1(z)
+        lx = self.dec1_label(l)
+        x = concatenate([x, lx])
         x = self.dec2(x)
         x_out = self.dec_out(x)
-        return Model(z, x_out)
+        return Model([z,l], x_out)
 
     def loss(self):
         bce = metrics.binary_crossentropy(self.x_input, self.x_out)
@@ -83,35 +95,55 @@ if __name__ == '__main__':
 
     batch_size = 32
     epochs = 2000
-    nd = 200 # number of images for training
+    nd = 1000 # number of images for training
+    ndo = 100 # number of images for testing
 
     # train the VAE on MNIST digits
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
     x_train = x_train.astype('float32') / 255.
     x_train = x_train[:nd]
-    n, h, w = x_train.shape
-    x_train = x_train.reshape(n, -1)
+    x_train = x_train.reshape(nd, -1)
+    y_train = y_train[:nd]
+    y_train = np.eye(10)[y_train] # to one-hot
+
+    x_test = x_test.astype('float32') / 255.
+    x_test = x_test[:ndo]
+    x_test = x_test.reshape(ndo, -1)
+    y_test = y_test[:ndo]
+    y_test = np.eye(10)[y_test] # to one-hot
 
     # make vae model and train
     np.random.seed(seed)
-    vae = VAE()
-    model = vae.vae()
+    cvae = CVAE()
+    model = cvae.cvae()
     model.summary()
-    model.add_loss(vae.loss())
+    model.add_loss(cvae.loss())
     np.random.seed(seed)
     model.compile(optimizer='adam', loss=None)
-    model.fit(x_train, epochs=epochs, batch_size=batch_size)
+    model.fit([x_train, y_train, y_train], epochs=epochs, batch_size=batch_size)
 
-    # decode images from 2-dim latent z
-    ndiv = 15 # number of images for axis
-    dec = vae.decoder()
-    gx = np.linspace(-3.0, 3.0, ndiv)
-    gxx, gyy = np.meshgrid(gx, gx[::-1])
-    z = np.append(gxx.reshape(-1,1), gyy.reshape(-1,1), axis=1)
-    x = dec.predict(z)
-    dst = x.reshape(ndiv,ndiv,28,28)
-    dst = dst.transpose(0,2,1,3)
-    dst = dst.reshape(ndiv*28, ndiv*28)
+    # extract latent z
+    pos = [] # select nu
+    for i in range(10):
+        n = x_train.shape[0]
+        mask = (np.argmax(y_train, axis=-1)==i)
+        pos.append(np.arange(n)[mask][0])
+
+    x = x_train[pos]
+    y = y_train[pos]
+    enc = cvae.encoder()
+    z = enc.predict([x,y])
+
+    # style gen
+    zz = z.repeat(10, axis=0)
+    yy = np.tile(np.eye(10), 10).T
+    dec = cvae.decoder()
+    xx = dec.predict([zz, yy])
+    xin = x.reshape(280,28)
+    xin = np.append(xin, np.zeros((280,5))+1.0, axis=1)
+    xout = xx.reshape(10,10,28,28).transpose(0,2,1,3).reshape(280,280)
+    dst = np.append(xin, xout, axis=1)
 
     # plot images
     plt.imshow(dst)
@@ -119,4 +151,5 @@ if __name__ == '__main__':
 
     # output images
     out = (cm.inferno(dst)[:,:,:3]*255).astype(np.uint8)
-    #skio.imsave('vae_out_%03d.png' % seed, out)
+    out[:,28:33] = 255
+    skio.imsave('png/cvae_out_%03d.png' % seed, out)
